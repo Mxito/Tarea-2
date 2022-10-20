4d98355183d6018029ba73ed94c8033a80372e0d
Iván Zapata <ivan.shinoda1308@gmail.com>
2022-10-20 16:15:20 -0300

    Custom Exceptions y Clase deposito terminada

--- TareaProg2/src/tareaprog2/claseDeposito.java
+++ TareaProg2/src/tareaprog2/claseDeposito.java
@@ -3,21 +3,26 @@ package tareaprog2;
 import java.util.ArrayList;
 
 class Deposito{
-    private ArrayList <Bebida> d;
+    private ArrayList<Bebida> D;
     private final int precio;
-    private final int vuelto;
+    private int vuelto;
     public Deposito(){
-      d = new ArrayList <Bebida>();
+      D = new ArrayList<>();
       precio = 1000;
       vuelto = 0;
     }
     public void addBebida(Bebida a){
-        d.add(a);
+        D.add(a);
     }
-    public Bebida getBebida(Moneda m){
-        if(d.size() ==0){
+    public Bebida getBebida(Moneda m) throws customException{
+        if(D.isEmpty()){
             return null;
         }
-        return d.remove(0);
+        if(m.getValor() < precio){
+            throw new customException("PagoInsuficienteException");
+        }else{
+            vuelto = m.getValor() - precio;
+            return D.remove(0);
+        }
     }
 }
